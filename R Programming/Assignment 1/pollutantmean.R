2c3d7f61c29855c4296bf9ceac99afeecb3c016e
paulofelipe <paulofel@hotmail.com>
2014-09-11 09:49:36 -0300

    r programming assignment 1

---
+++ R Programming/Assignment 1/pollutantmean.R
@@ -0,0 +1,14 @@
+setwd("D:/Users/paulo.alencar/Downloads/rprog_data_specdata/") # Set working directory to specdata
+
+pollutantmean <- function(directory,pollutant,id=1:332){
+ 
+  files <- list.files(directory)
+  sum <- 0
+  n <- 0
+  for (i in 1:length(id)){
+    dados<-read.csv(paste(directory,"/",files[id[i]],sep=""))
+    sum <- sum + sum(dados[,pollutant],na.rm=T)
+    n <- n + sum(!is.na(dados[,pollutant]))
+  }
+  return(sum/n)
+}
